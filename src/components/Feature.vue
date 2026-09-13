90ef89f096e1e325076e7572cd70db6c37d178e9
Zornitsa Serbezova <zornitsa.serbezova@live.com>
2026-09-13 11:24:14 +0300

    feature

--- src/components/Feature.vue
+++ src/components/Feature.vue
@@ -3,7 +3,21 @@ defineProps({ blok: Object });
 </script>
 
 <template>
-  <div class="feature" v-editable="blok">
-    <span>{{ blok.name }}</span>
+  <a
+    v-if="blok.link?.url"
+    v-editable="blok"
+    :href="blok.link.url"
+    class="feature"
+  >
+    {{ blok.name }}
+  </a>
+
+  <div v-else v-editable="blok" class="feature">
+    {{ blok.name }}
   </div>
 </template>
+
+<script setup>
+defineProps({ blok: Object })
+.feature { display: block; color: inherit; text-decoration: none; cursor: pointer; }
+</script>
